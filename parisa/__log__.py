32878dc6db32af44881453765e24472756a0540f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:32:29 -0300

    Created parisa/__log__.py @2020-10-26 19:32

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 26 2020 16:32:27.305 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  PAISAGEM = https://i.imgur.com/l2mTRWE.jpg
+                   ^
+SyntaxError: invalid syntax
+'''},
