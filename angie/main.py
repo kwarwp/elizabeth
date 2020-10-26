f85eed21fded56ed5e520f6b061bb3759f97a414
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:51:34 -0300

    Saved angie/main.py @2020-10-26 19:51

--- angie/main.py
+++ angie/main.py
@@ -5,15 +5,17 @@ CASTELO = "https://i.imgur.com/LF4NbKH.jpg"
 PEIXE = "https://i.imgur.com/uw3Q4kO.png"
 DESERTO = "https://i.imgur.com/AOysKp6.jpeg"
 MORANGO= "https://i.imgur.com/TYqfeqN.png"
+JOHN= "https://i.imgur.com/nuaU01Hb.jpg"
 
 castelo = Cena (img=CASTELO) 
-peixe = Elemento (img=PEIXE)
+peixe = Elemento (img=PEIXE, tit = "ooi")
 peixe.entra(castelo) 
 deserto = Cena (img=DESERTO)
 castelo.direita=deserto
 deserto.direita=castelo
 morango= Elemento (img=MORANGO)
 morango.entra(deserto)
-
+#john = Elemento (img= JOHN)
+#john.entra(castelo)
 
 castelo.vai()
