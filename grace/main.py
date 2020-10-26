3dd3f5ee6ff7a90af860c8e5b0241c7390a2ef74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:58:35 -0300

    Saved grace/main.py @2020-10-26 19:58

--- grace/main.py
+++ grace/main.py
@@ -5,16 +5,17 @@ CASTELO = "https://i.imgur.com/LF4NbKH.jpg"
 PEIXE = "https://i.imgur.com/uw3Q4kO.png"
 DESERTO = "https://i.imgur.com/AOysKp6.jpeg"
 MORANGO = "https://i.imgur.com/TYqfeqN.png"
+TUBARAO_VIVO = "https://i.imgur.com/1tfjX56.png"
 
 castelo= Cena(img=CASTELO)
-peixe=Elemento(img=PEIXE)
+peixe=Elemento(img=PEIXE, tit ="ooi", top=500, heigth = 9000, widht= 2000, left=1000)
 peixe.entra(castelo)
 deserto= Cena(img = DESERTO)
 castelo.direita=deserto
 deserto.esquerda=castelo
-#morango= Elemento(img=MORANGO)
-
-
-
+morango= Elemento(img=MORANGO,tit ="me chamo morango")
+morango.entra(deserto)
+#tubarao_vivo =Elemento(img=TUBARAO_VIVO)
+#tubarao_vivo.entra(deserto)
 
 castelo.vai()
