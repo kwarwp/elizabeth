f811c53cfcf002d19402a2a654e5c37a31034883
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:36:42 -0300

    Saved grace/main.py @2020-10-26 19:36

--- grace/main.py
+++ grace/main.py
@@ -1,1 +1,20 @@
 # elizabeth.grace.main.py
+from _spy.vitollino.main import Cena, Elemento
+
+CASTELO = "https://i.imgur.com/LF4NbKH.jpg"
+PEIXE = "https://i.imgur.com/uw3Q4kO.png"
+DESERTO = "https://i.imgur.com/AOysKp6.jpeg"
+MORANGO = "https://i.imgur.com/TYqfeqN.png"
+
+castelo= Cena(img=CASTELO)
+peixe=Elemento(img=PEIXE)
+peixe.entra(castelo)
+deserto= Cena(img = DESERTO)
+castelo.direita=deserto
+deserto.esquerda=castelo
+#morango= Elemento(img=MORANGO)
+
+
+
+
+castelo.vai()
