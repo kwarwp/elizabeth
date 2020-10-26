7ea7f803ea730615886fcfaf0005ff2bad50bc7a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:32:49 -0300

    Saved parisa/__log__.py @2020-10-26 19:32

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -6,3 +6,10 @@
                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 26 2020 16:32:49.465 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  DESERTO = 
+             ^
+SyntaxError: invalid syntax
+'''},
