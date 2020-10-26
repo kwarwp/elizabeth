cddde583177683ed86636ae7f142ade2bb8c7481
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:56:30 -0300

    Saved ruzwana/__log__.py @2020-10-26 19:56

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -173,3 +173,19 @@ NameError: name 'DESERTO' is not defined
                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 26 2020 16:56:27.979 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    tubarao_vivo=Elemento(img=TUBARAO_VIVO)
+NameError: name 'TUBARAO_VIVO' is not defined
+'''},
