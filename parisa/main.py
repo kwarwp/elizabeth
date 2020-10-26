bb66e1814292085c3ce24690ed01b7632002aadd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:58:44 -0300

    Saved parisa/main.py @2020-10-26 19:58

--- parisa/main.py
+++ parisa/main.py
@@ -3,11 +3,13 @@ from _spy.vitollino.main import Cena, Elemento
 
 CASTELO = "https://i.imgur.com/LF4NbKH.jpg"
 MORANGO = "https://i.imgur.com/TYqfeqN.png"
- 
+DESERTO = "https://i.imgur.com/AOysKp6.jpg" 
 
-castelo= Cena (img=CASTELO)
-morango=Elemento(img=MORANGO)
+ 
+castelo=Cena (img=CASTELO)
+morango= Elemento(img=MORANGO,tit ="oi")
 morango.entra(castelo)
+castelo.vai()
 
 
 
