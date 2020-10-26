0c16bd143573a4acf3b13204344623c6860f89e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:55:18 -0300

    Saved grace/__log__.py @2020-10-26 19:55

--- grace/__log__.py
+++ grace/__log__.py
@@ -6,3 +6,10 @@
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 26 2020 16:55:17.340 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  peixe=Elemento(img=PEIXE, tit ="ooi", x= 90,y= 50,w=100 e h=800px)
+                                                           ^
+SyntaxError: invalid syntax
+'''},
