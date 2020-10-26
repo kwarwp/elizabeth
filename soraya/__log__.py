0d1bc613f1f7339c7375e1de8d3baedfe03b12e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:53:52 -0300

    Saved soraya/__log__.py @2020-10-26 19:53

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -86,3 +86,19 @@ TypeError: entra() takes 3 positional argument but more were given
                                 ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Mon Oct 26 2020 16:53:52.311 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    morango = Elemento(img=morango,x=100,y=100)
+NameError: name 'morango' is not defined
+'''},
