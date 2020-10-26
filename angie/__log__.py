48060d7cbdb6fa2548af043c48a6834a1defbdd4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:27:48 -0300

    Created angie/__log__.py @2020-10-26 19:27

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 26 2020 16:27:45.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  peixe.entra(castelo) direita 
+                        ^
+SyntaxError: invalid syntax
+'''},
