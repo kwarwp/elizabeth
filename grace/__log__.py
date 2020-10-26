7ed98cae11a0c5f4b3db7b8c8c9b9a73dd4fbc9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:27:08 -0300

    Created grace/__log__.py @2020-10-26 19:27

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 26 2020 16:27:06.419 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  CASTELO = https://i.imgur.com/LF4NbKH.jpg
+                  ^
+SyntaxError: invalid syntax
+'''},
