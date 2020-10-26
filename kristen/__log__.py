19b536cc9c9b3ec4c23a61c4d46a013b9fc3ae9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:58:33 -0300

    Created kristen/__log__.py @2020-10-26 19:58

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 26 2020 16:58:31.739 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  tubarao_vivo = Elemento(img=TUBARAO_VIVO,
+                                                                         ^
+SyntaxError: invalid syntax
+'''},
