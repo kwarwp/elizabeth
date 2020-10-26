63dbd6025dada34eb26d7eae34560f023271f057
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:36:33 -0300

    Created parisa/main.py @2020-10-26 19:36

---
+++ parisa/main.py
@@ -0,0 +1,12 @@
+# elizabeth.parisa.main.py
+from _spy.vitollino.main import Cena, Elemento
+
+CASTELO = "https://i.imgur.com/LF4NbKH.jpg"
+MORANGO = "https://i.imgur.com/TYqfeqN.png"
+ 
+
+paisagem= Cena (img=PAISAGEM)
+morango=Elemento(img=MORANGO)
+morango.entra(paisagem)
+
+
