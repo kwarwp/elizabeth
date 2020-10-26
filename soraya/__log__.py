42f6ca1e23a07a983b28b4f455f1f7bd8005cbad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 16:25:26 -0300

    Saved soraya/__log__.py @2020-10-26 19:25

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -15,3 +15,19 @@
     peixe.entra()
 TypeError: entra missing 1 positional argument: 'cena'
 '''},
+{'date': 'Mon Oct 26 2020 16:25:26.466 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    peixe.entra(castelo,x=10)
+TypeError: entra() got an unexpected keyword argument 'x'
+'''},
