2f40c949127fbdafdf5f110e20af7883525937b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 16:54:48 -0300

    Saved libby/__log__.py @2020-12-04 19:54

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,21 @@
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Dec 04 2020 16:54:47.667 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 89
+    desafio_2().inicia()
+  module <module> line 39
+    self.texto_1 = Texto(cena1, txt = "Encontre o livro")
+NameError: name 'cena1' is not defined
+'''},
