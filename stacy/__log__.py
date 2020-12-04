4a3139b92edc1a96498ca3f2ed172136a3b0a87b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 17:11:48 -0300

    Saved stacy/__log__.py @2020-12-04 20:11

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -15,3 +15,21 @@
     desafio_2().inicia()
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Fri Dec 04 2020 17:11:47.816 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    mari().chamaa()
+  module <module> line 11
+    desafio_2.inicia()
+AttributeError: 'module' object has no attribute 'inicia'
+'''},
