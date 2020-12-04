abc77ef03072d2947680eaf81b5504adf7df431e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 16:21:46 -0300

    Created libby/__log__.py @2020-12-04 19:21

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Dec 04 2020 16:21:44.757 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  def __init__()
+                 ^
+SyntaxError: invalid syntax
+'''},
