461e6f5bba28e8c36403a3bdce880697e2e694bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 17:13:34 -0300

    Created stacy/main.py @2020-12-04 20:13

---
+++ stacy/main.py
@@ -0,0 +1,17 @@
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+from elizabeth.libby import desafio_2
+
+
+class mari:
+
+    def __init__(self):
+        pass
+        
+    def chamaa(self):
+        desafio_2().inicia()
+
+
+
+
+if __name__ == "__main__":                  
+    mari().chamaa()
