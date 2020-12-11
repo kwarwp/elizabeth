5208282390ed3b8db946ab470ad0cd554f3d8ac7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 20:22:09 -0300

    Saved libby/__log__.py @2020-12-11 23:22

--- libby/__log__.py
+++ libby/__log__.py
@@ -267,3 +267,19 @@ Traceback (most recent call last):
   module libby.main line 14
     from meredith.main import nome_personagem
 '''},
+{'date': 'Fri Dec 11 2020 20:22:07.770 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from meredith.main import nome_personagem
+ImportError: cannot import name 'nome_personagem'
+'''},
