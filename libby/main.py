59146d5eb2fcd54b67c8db857c0e4ed0c13a8781
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 19:59:15 -0300

    Saved libby/main.py @2020-12-11 22:59

--- libby/main.py
+++ libby/main.py
@@ -11,7 +11,7 @@ Changelog
 
 
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
-from meredith.main import *
+from meredith.main import nome_personagem
 #from cenas.imix import Inicial
 #from cenas.ik import Passeio
         
