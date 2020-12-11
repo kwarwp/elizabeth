9306f17168ccfce0dee4a07c592476bebe413b3b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 17:29:36 -0300

    Saved roxanne/__log__.py @2020-12-11 20:29

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -212,3 +212,23 @@ IndentationError: expected an indented block
     (nome_personagem)
 NameError: name 'nome_personagem' is not defined
 '''},
+{'date': 'Fri Dec 11 2020 17:29:34.978 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    teste1()
+  module <module> line 28
+    print(f'nomeperkdad {nome_personagem}')
+  module <module> line 1
+    (nome_personagem)
+NameError: name 'nome_personagem' is not defined
+'''},
