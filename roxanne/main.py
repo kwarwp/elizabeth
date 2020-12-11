63a1137acbcd1503f69c4878e6bd07d77c31b972
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 17:28:33 -0300

    Saved roxanne/main.py @2020-12-11 20:28

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,6 +1,8 @@
 # elizabeth.roxanne.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
+#class parte1:
+
 imagem_play= 'https://img2.gratispng.com/20180325/fyw/kisspng-youtube-play-button-computer-icons-clip-art-button-5ab819cc3d5ee6.2062185015220146682514.jpg'
 imagem_pc='https://cdn5.colorir.com/desenhos/color/201049/773d6796c883c528d45a21258f54e263.png'
 fundo_transparente='https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.techtudo.com.br%2Fdicas-e-tutoriais%2Fnoticia%2F2012%2F11%2Fcomo-remover-o-fundo-de-fotos-e-deixa-lo-transparente-no-photoscape.html&psig=AOvVaw36qwHy3_KTk7xvMzE0pLBr&ust=1607800797505000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiZ2vDSxu0CFQAAAAAdAAAAABAD'
@@ -13,17 +15,22 @@ PC=Cena(imagem_pc)
 
 def nomepersonagem(nome_personagem):
     nome_personagem= str(input('Escolha o nome da personagem:'))
+    
     if nome_personagem == None:
         nome_personagem=fulana
-        PC.vai()
         print(nome_personagem)
+        PC.vai()
     else:
         nome_personagem= nome_personagem
         PC.vai()
-        print(nome_personagem)
+        
+def teste1():
+    print(f'nomeperkdad {nome_personagem}')
+        
     
     
 PLAY.vai()
 BOTAO_PLAY.elt.bind("click", nomepersonagem)
+teste1()
 
 
