687ecefd66302da6fb208dd87354295652119578
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 18:17:24 -0300

    Saved meredith/__log__.py @2020-12-11 21:17

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -52,3 +52,21 @@ SyntaxError: invalid syntax
     nome_personagem= pergunta_nome
 NameError: name 'pergunta_nome' is not defined
 '''},
+{'date': 'Fri Dec 11 2020 18:17:22.739 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    Entrada().start_eng()
+  module <module> line 36
+    BOTAO_PLAY= Elemento(imagem_play, tit= "click" ,cena = PLAY)
+NameError: name 'PLAY' is not defined
+'''},
