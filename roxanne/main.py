fe9511ed54f2fea6697ea95243436e9afc229df7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 17:42:15 -0300

    Saved roxanne/main.py @2020-12-11 20:42

--- roxanne/main.py
+++ roxanne/main.py
@@ -16,7 +16,7 @@ nome_personagem=''
 def nomepersonagem(pergunta_nome):
     global nome_personagem
     pergunta_nome= str(input('Escolha o nome da personagem:'))
-    nome_personagem= pergunta_nome
+    nome_personagem=+ pergunta_nome
     if nome_personagem == None:
         nome_personagem=fulana
         print(nome_personagem)
@@ -24,8 +24,9 @@ def nomepersonagem(pergunta_nome):
     else:
         nome_personagem= nome_personagem
         PC.vai()
+        print(nome_personagem)
         
-texto_ = Texto(PC, txt = f'nomeperkdad {nome_personagem}')
+#texto_ = Texto(PC, txt = f'nomeperkdad {nome_personagem}')
 
 
         
@@ -33,6 +34,6 @@ texto_ = Texto(PC, txt = f'nomeperkdad {nome_personagem}')
     
 PLAY.vai()
 BOTAO_PLAY.elt.bind("click", nomepersonagem)
-texto_.vai()
+
 
 
