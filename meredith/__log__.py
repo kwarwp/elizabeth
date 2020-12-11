245f13e440807f9dc9554dbc4d7cc9ed6841aebb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 16:03:41 -0300

    Created meredith/__log__.py @2020-12-11 19:03

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Dec 11 2020 16:03:38.828 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  print("Eu fui à feira com a" diga_o_nome)
+                                ^
+SyntaxError: invalid syntax
+'''},
