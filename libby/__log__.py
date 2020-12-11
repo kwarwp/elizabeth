79f940ccc1d82eeb3679040a5be194af209bf625
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 20:42:16 -0300

    Saved libby/__log__.py @2020-12-11 23:42

--- libby/__log__.py
+++ libby/__log__.py
@@ -340,3 +340,23 @@ Traceback (most recent call last):
     from meredith.main import nome_personagem
 ImportError: cannot import name 'nome_personagem'
 '''},
+{'date': 'Fri Dec 11 2020 20:42:14.304 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 82
+    desafio_2().inicia()
+  module <module> line 39
+    self.texto_1 = Texto(self.cena1, txt = f'{nome_personagem},Encontre o livro')
+  module <module> line 1
+    (nome_personagem)
+NameError: name 'nome_personagem' is not defined
+'''},
