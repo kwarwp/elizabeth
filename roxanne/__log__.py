0a0cfb8af81ca614f49b7dae87602e61c25ffe08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 17:32:07 -0300

    Saved roxanne/__log__.py @2020-12-11 20:32

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -232,3 +232,23 @@ NameError: name 'nome_personagem' is not defined
     (nome_personagem)
 NameError: name 'nome_personagem' is not defined
 '''},
+{'date': 'Fri Dec 11 2020 17:32:05.525 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    teste1()
+  module <module> line 28
+    texto_ = Texto(PC, txt = f'nomeperkdad {nome_personagem}')
+  module <module> line 1
+    (nome_personagem)
+NameError: name 'nome_personagem' is not defined
+'''},
