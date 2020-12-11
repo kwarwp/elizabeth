2b6939fa0008a0e6d08c5afe76ba700a1b9b91ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 16:14:41 -0300

    Created roxanne/__log__.py @2020-12-11 19:14

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Dec 11 2020 16:14:38.579 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  PLAY..elt.bind("click", nomepersonagem)
+        ^
+SyntaxError: invalid syntax
+'''},
