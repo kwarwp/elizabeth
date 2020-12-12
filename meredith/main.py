a22a948c8f966f119ce0b5081d8eb73247d96177
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 21:20:58 -0300

    Saved meredith/main.py @2020-12-12 00:20

--- meredith/main.py
+++ meredith/main.py
@@ -15,18 +15,24 @@ imagem_play= 'https://img2.gratispng.com/20180325/fyw/kisspng-youtube-play-butto
 imagem_pc='https://cdn5.colorir.com/desenhos/color/201049/773d6796c883c528d45a21258f54e263.png'
 fundo_transparente='https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.techtudo.com.br%2Fdicas-e-tutoriais%2Fnoticia%2F2012%2F11%2Fcomo-remover-o-fundo-de-fotos-e-deixa-lo-transparente-no-photoscape.html&psig=AOvVaw36qwHy3_KTk7xvMzE0pLBr&ust=1607800797505000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiZ2vDSxu0CFQAAAAAdAAAAABAD'
 
+class Registro:
 
-pergunta_nome= str(input('Escolha o nome da personagem:'))
-if pergunta_nome == '':
-   pergunta_nome= "fulana"
-else:
-   pergunta_nome = pergunta_nome
-
-nome_personagem = pergunta_nome
-
+    def __init__(self):
+        pass
+        
+    def pergunta_nome(self):
+        self.pergunta_nome= str(input('Escolha o nome da personagem:'))
+        if self.pergunta_nome == '':
+           self.pergunta_nome = "fulana"
+        else:
+           self.pergunta_nome = self.pergunta_nome
+        self.nome_personagem = self.pergunta_nome   
+    def inicia_jogo(self):
+         self.pergunta_nome()
+         Entrada().start_eng()
 
 class Entrada:
-  
+
     def __init__(self):
         self.PLAY= Cena(imagem_play)
         self.BOTAO_PLAY= Elemento(imagem_play, tit= "click" ,cena = self.PLAY)
@@ -40,12 +46,12 @@ class Entrada:
         self.texto_.vai()
             
     def start_eng(self):
-    
         self.PLAY.vai()
+        
     def viaja_para_libby(self):
         from  libby.main import desafio_2
         desafio_2().inicia()
         
 if __name__ == "__main__":                  
-    Entrada().start_eng()
+    Registro().inicia_jogo()
             
