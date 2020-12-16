4786de12bf4e3d9b8a96662c33341239a4c15c1d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-16 17:41:46 -0300

    Created courtney/main.py @2020-12-16 20:41

---
+++ courtney/main.py
@@ -0,0 +1,35 @@
+# elizabeth.courtney.main.py
+
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+
+Imagem_Biblioteca_porta="https://media.istockphoto.com/vectors/closed-door-drawing-vector-id865663698"
+Imagem_Biblioteca_porta2="https://image.freepik.com/vetores-gratis/porta-aberta-dos-desenhos-animados-entrada-do-corredor-do-apartamento-entrada-do-escritorio_53562-8532.jpg"
+Imagem_Biblioteca_dentro="https://secult.es.gov.br/Media/secult/BPES/IMG_7650.JPG"
+
+STYLE["width"] = 500 #  width = 300 (default)
+STYLE["heigth"] = '600px' # min-height = "300px"
+
+Biblioteca_entrada=Cena(Imagem_Biblioteca_porta)
+Biblioteca_entrada.vai()
+
+Biblioteca_porta=Cena(Imagem_Biblioteca_porta2)
+
+Biblioteca_dentro=Cena(Imagem_Biblioteca_dentro)
+
+Imagem_botao="https://images-na.ssl-images-amazon.com/images/I/71nQDXqkyDL.png"
+
+def desafio_porta1 (event = None):
+    Biblioteca_porta.vai()
+    
+BOTAO= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                cena = Biblioteca_entrada)
+                                
+BOTAO.elt.bind("click", desafio_porta1)
+
+def desafio_porta2 (event = None):
+    Biblioteca_dentro.vai()
+    
+BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                cena = Biblioteca_porta)
+                                
+BOTAO2.elt.bind("click", desafio_porta2)
