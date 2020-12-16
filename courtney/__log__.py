8a6d0be55ba93c720d7061789fc8f5b453e7429f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-16 18:05:07 -0300

    Created courtney/__log__.py @2020-12-16 21:05

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 16 2020 18:05:04.352 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  botao_caixa = Elemento(botao_seta, tit="CLIQUE",
+  ^
+IndentationError: unexpected indent
+'''},
