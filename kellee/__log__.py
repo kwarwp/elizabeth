0c6ed089e5303a93f4a1034ca421b7ec1c167bfc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-16 16:26:50 -0300

    Created kellee/__log__.py @2020-12-16 19:26

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 16 2020 16:26:48.105 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  PERSONAGEM_DORMINDO= Elemento(imagem_personagem1, tit="Acorde",
+                                                                                                                                                                                                                                                  ^
+SyntaxError: invalid syntax
+'''},
