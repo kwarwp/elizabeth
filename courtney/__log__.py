72ff22981527a84d62a01aba44ff795cd335c9bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-16 18:06:17 -0300

    Saved courtney/__log__.py @2020-12-16 21:06

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Dec 16 2020 18:06:15.976 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  botao_caixa = Elemento(Imagem_Biblioteca_caixa, tit="CLIQUE",
+  ^
+IndentationError: unexpected indent
+'''},
