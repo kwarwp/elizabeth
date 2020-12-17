926c5624683fd2013360d00d7d8ecec5763b83e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-16 22:54:14 -0300

    Created natalia/__log__.py @2020-12-17 01:54

---
+++ natalia/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Wed Dec 16 2020 22:54:10.446 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    Bloco(OCEANO)
+  module <module> line 29
+    Suporte(self, "folha" + fl, pos//4, pos%4)
+NameError: name 'Suporte' is not defined
+'''},
