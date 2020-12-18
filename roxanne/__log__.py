575525ae20516c52c3d0dee5e00dcb5831c835ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 16:27:55 -0300

    Saved roxanne/__log__.py @2020-12-18 19:27

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -988,3 +988,20 @@ ImportError: cannot import name 'Modulo1'
 'error': '''
 Exception: <TypeError: obj is null>
 '''},
+{'date': 'Fri Dec 18 2020 16:27:54.524 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Qualquer palhaçada
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    INICIA.nome_personagem
+AttributeError: 'NoneType' object has no attribute 'nome_personagem'
+'''},
