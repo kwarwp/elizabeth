0ea6fb4a2c32e0c1ace51db7fd873a680a962049
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 20:16:09 -0300

    Saved stacy/__log__.py @2020-12-18 23:16

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -219,3 +219,34 @@ Exception: obj is null
   module '$exec_1850' line 2
 from roxanne.main import *
 '''},
+{'date': 'Fri Dec 18 2020 20:16:08.19 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from roxanne.main import instancia.nome_personagem
+ImportError: cannot import name 'instancia'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from roxanne.main import instancia.nome_personagem
+'''},
