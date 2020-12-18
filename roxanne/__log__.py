f34c70ec78da079b0b907913c5a123ade8e8afc5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 15:32:31 -0300

    Saved roxanne/__log__.py @2020-12-18 18:32

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -809,3 +809,21 @@ NameError: name 'chama_mod1' is not defined
     self.bot1 = Elemento(comecador, tit="CLIQUE PARA COMEÇAR",
 AttributeError: 'Inicia' object has no attribute 'Cena2'
 '''},
+{'date': 'Fri Dec 18 2020 15:32:31.97 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    Inicia().gera()
+  module <module> line 79
+    self.cena.vai()
+AttributeError: 'GUI' object has no attribute 'cena'
+'''},
