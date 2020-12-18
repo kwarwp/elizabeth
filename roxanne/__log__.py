cca6e182eb6e0c398f8321837b0eb4db26a9a107
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 15:04:02 -0300

    Saved roxanne/__log__.py @2020-12-18 18:04

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -754,3 +754,22 @@ AttributeError: 'NoneType' object has no attribute 'nome_personagem'
                         ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Dec 18 2020 15:03:59.566 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''True fulana
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    Inicia().cria_usuario()
+  module <module> line 58
+    if nome== True:
+NameError: name 'nome' is not defined
+'''},
