abfe0e1d79eed007e0291d444319e38dfbccf769
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 14:34:24 -0300

    Saved roxanne/__log__.py @2020-12-18 17:34

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -493,3 +493,21 @@ NameError: name '_Inicia__nome' is not defined
     if Resgistro.nome == True:
 NameError: name 'Resgistro' is not defined
 '''},
+{'date': 'Fri Dec 18 2020 14:34:21.547 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    Inicia().cria_usuario()
+  module <module> line 48
+    if Registro.nome == True:
+AttributeError: 'Registro' object has no attribute 'nome'
+'''},
