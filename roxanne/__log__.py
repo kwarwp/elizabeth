a6fafe45105c2a94c5004cbdc4af79870f59d6b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 16:45:56 -0300

    Saved roxanne/__log__.py @2020-12-18 19:45

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1181,3 +1181,19 @@ SyntaxError: invalid syntax
     if ticket == True:
 NameError: name 'ticket' is not defined
 '''},
+{'date': 'Fri Dec 18 2020 16:45:55.477 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    if ticket == True:
+NameError: name 'ticket' is not defined
+'''},
