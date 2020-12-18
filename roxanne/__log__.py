1ff27a667d072fe0f5388f86f48c8ac5baa69e46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 19:01:32 -0300

    Saved roxanne/__log__.py @2020-12-18 22:01

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1406,3 +1406,23 @@ Traceback (most recent call last):
   module roxanne.main line 15
     from stacy.main import Modulo1
 '''},
+{'date': 'Fri Dec 18 2020 19:01:30.865 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    Inicia().cria_persona()
+  module <module> line 52
+    self.bot1 = Elemento(comecador, tit="CLIQUE PARA COMEÇAR",
+  module <module> line 59
+    print(self.nome_personagem)
+AttributeError: 'Inicia' object has no attribute 'nome_personagem'
+'''},
