27c3a241a741349788baa47c2e2e04a7d5508e6f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 14:57:31 -0300

    Saved roxanne/__log__.py @2020-12-18 17:57

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -697,3 +697,20 @@ IndentationError: expected an indented block
                         ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Dec 18 2020 14:57:28.665 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''True DFF
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    print(VAI, VAI.nome)
+AttributeError: 'NoneType' object has no attribute 'nome'
+'''},
