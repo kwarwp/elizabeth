32edc7bb6b1eab9651c65064785dbdd79097d1ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 19:03:41 -0300

    Saved roxanne/__log__.py @2020-12-18 22:03

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1442,3 +1442,19 @@ AttributeError: 'Inicia' object has no attribute 'nome_personagem'
     Inicia().registra()
 AttributeError: 'Inicia' object has no attribute 'registra'
 '''},
+{'date': 'Fri Dec 18 2020 19:03:40.137 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    Inicia().registra()
+AttributeError: 'Inicia' object has no attribute 'registra'
+'''},
