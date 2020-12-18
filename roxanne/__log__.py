d2ce13dfb993406d66aa999e8505ecc3585888db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 14:35:04 -0300

    Saved roxanne/__log__.py @2020-12-18 17:35

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -511,3 +511,19 @@ NameError: name 'Resgistro' is not defined
     if Registro.nome == True:
 AttributeError: 'Registro' object has no attribute 'nome'
 '''},
+{'date': 'Fri Dec 18 2020 14:35:01.966 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    print(Registro.nome)
+AttributeError: 'Registro' object has no attribute 'nome'
+'''},
