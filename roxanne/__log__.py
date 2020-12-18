077ea4ffe114e8ec6e7534ee2618359cf0ec5960
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 18:35:23 -0300

    Saved roxanne/__log__.py @2020-12-18 21:35

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1347,3 +1347,23 @@ AttributeError: 'Inicia' object has no attribute 'nome'
     print(TESTE.nome, TESTE.nome_personagem)
 AttributeError: 'Inicia' object has no attribute 'nome'
 '''},
+{'date': 'Fri Dec 18 2020 18:35:21.874 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    from stacy.main import Modulo1
+  module stacy.main line 2
+    from roxanne.main import Registro
+  module roxanne.main line 67
+    print(TESTE.nome, TESTE.nome_personagem)
+AttributeError: 'Inicia' object has no attribute 'nome'
+'''},
