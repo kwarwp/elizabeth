798f2e1c0b33e78ff661392fe73103658f5f539b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 16:58:46 -0300

    Saved roxanne/__log__.py @2020-12-18 19:58

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1281,3 +1281,20 @@ AttributeError: 'NoneType' object has no attribute 'nome_personagem'
     INSTANCIA.nome
 AttributeError: 'NoneType' object has no attribute 'nome'
 '''},
+{'date': 'Fri Dec 18 2020 16:58:45.230 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''True fulana
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    print(INSTANCIA.nome, INSTANCIA.nome_personagem)
+NameError: name 'INSTANCIA' is not defined
+'''},
