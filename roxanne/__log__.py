a06d6267761428013ccd10ed20f775b373848547
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 15:39:02 -0300

    Saved roxanne/__log__.py @2020-12-18 18:39

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -827,3 +827,42 @@ AttributeError: 'Inicia' object has no attribute 'Cena2'
     self.cena.vai()
 AttributeError: 'GUI' object has no attribute 'cena'
 '''},
+{'date': 'Fri Dec 18 2020 15:39:01.903 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    from stacy.main import Modulo1
+  module stacy.main line 2
+    from roxanne.main import nome_personagem
+  module roxanne.main line 15
+    from stacy.main import Modulo1
+ImportError: cannot import name 'Modulo1'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    from stacy.main import Modulo1
+  module stacy.main line 2
+    from roxanne.main import nome_personagem
+  module roxanne.main line 15
+    from stacy.main import Modulo1
+'''},
