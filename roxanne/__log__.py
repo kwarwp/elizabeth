38b3d2ce05ce5271486600be7fbfeca504fa2857
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 14:52:52 -0300

    Saved roxanne/__log__.py @2020-12-18 17:52

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -650,3 +650,22 @@ NameError: name 'regista' is not defined
     print(registra.nome, self.nome_personagem) 
 NameError: name 'registra' is not defined
 '''},
+{'date': 'Fri Dec 18 2020 14:52:49.800 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''True fsdf
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    Inicia().cria_usuario()
+  module <module> line 51
+    if nome== True:
+NameError: name 'nome' is not defined
+'''},
