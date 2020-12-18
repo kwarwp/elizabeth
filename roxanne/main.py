de6b64790c668adc5ae1e18f64ed293910f96298
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 14:59:09 -0300

    Saved roxanne/main.py @2020-12-18 17:59

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,42 +1,61 @@
 # elizabeth.roxanne.main.py
+"""     xxxxx
+
+.. codeauthor:: Emanuelle Simas <ellesimas@gmail.com>
+
+Changelog
+---------
+.. versionadded::    
+       - Class registro
+
+"""
+
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+from stacy.main import Modulo1
 
 #class parte1:
 
 imagem_play= 'https://img2.gratispng.com/20180325/fyw/kisspng-youtube-play-button-computer-icons-clip-art-button-5ab819cc3d5ee6.2062185015220146682514.jpg'
 imagem_pc='https://cdn5.colorir.com/desenhos/color/201049/773d6796c883c528d45a21258f54e263.png'
-fundo_transparente='https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.techtudo.com.br%2Fdicas-e-tutoriais%2Fnoticia%2F2012%2F11%2Fcomo-remover-o-fundo-de-fotos-e-deixa-lo-transparente-no-photoscape.html&psig=AOvVaw36qwHy3_KTk7xvMzE0pLBr&ust=1607800797505000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiZ2vDSxu0CFQAAAAAdAAAAABAD'
+tela_branca = "http://www.f12com.com.br/wp-content/uploads/tela_branca.jpg"
 
 
-PLAY= Cena(imagem_play)
-BOTAO_PLAY= Elemento(imagem_play, tit= "click" ,cena = PLAY)
+class Registro:
 
-PC=Cena(imagem_pc)
-nome_personagem=''
-#def nomepersonagem(pergunta_nome):
-    
-pergunta_nome= str(input('Escolha o nome da personagem:'))
-nome_personagem= pergunta_nome
-if nome_personagem == None:
-    nome_personagem=fulana
-    print(nome_personagem)
-#        PC.vai()
-else:
-    nome_personagem= nome_personagem
-    #    PC.vai()
-    print(nome_personagem)
+    def __init__(self, nome = False):
+        self.nome = nome
         
-
+    def registra(self):
+        self.pergunta_nome= str(input('Escolha o nome da personagem:'))
+        if self.pergunta_nome == '':
+            self.pergunta_nome = "fulana"
+            self.nome = True
+        else:
+            self.pergunta_nome = self.pergunta_nome
+            self.nome = True  
+        self.nome_personagem = self.pergunta_nome   
+        """ testa se após passar por aqui, self.nome se torna true e a variável grava o nome """
+        #print(self.nome, self.nome_personagem) 
         
-texto_ = Texto(PLAY, txt = nome_personagem + 'Gosta de ler um livro')
+VAI = Registro().registra()
+VAI.nome_personagem
 
 
         
-    
-    
-PLAY.vai()
-#BOTAO_PLAY.elt.bind("click", nomepersonagem)
-texto_.vai()
+class Inicia:
 
-
-
+    def __init__(self, ticket = False):
+        self.ticket = ticket
+        if self.ticket == True:
+            Modulo1().mari()
+        
+        
+    def cria_usuario(self):
+        TESTE = Registro().registra()
+        if nome== True:
+            self.ticket = True
+            
+            
+#if __name__ == "__main__":    
+    #Inicia().cria_usuario()
+    #Registro().registra()
