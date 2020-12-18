3da0394c37d2db09109102bb2f645f15e36b0595
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 16:17:09 -0300

    Saved stacy/__log__.py @2020-12-18 19:17

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -154,3 +154,23 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Fri Dec 18 2020 16:17:08.744 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    Modulo1().mari()
+  module <module> line 37
+    print(f'Testando {nome_personagem}')
+  module <module> line 1
+    (nome_personagem)
+NameError: name 'nome_personagem' is not defined
+'''},
