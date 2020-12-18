010a424b8f88c6edaab87b77b82790da40df679a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-18 15:34:33 -0300

    Saved roxanne/main.py @2020-12-18 18:34

--- roxanne/main.py
+++ roxanne/main.py
@@ -7,6 +7,7 @@ Changelog
 ---------
 .. versionadded::    
        - Class registro
+       - Class inicia 
 
 """
 
@@ -18,6 +19,8 @@ from stacy.main import Modulo1
 imagem_play= 'https://img2.gratispng.com/20180325/fyw/kisspng-youtube-play-button-computer-icons-clip-art-button-5ab819cc3d5ee6.2062185015220146682514.jpg'
 imagem_pc='https://cdn5.colorir.com/desenhos/color/201049/773d6796c883c528d45a21258f54e263.png'
 tela_branca = "http://www.f12com.com.br/wp-content/uploads/tela_branca.jpg"
+registrador = "https://www.freeiconspng.com/uploads/register-button-png-0.png"
+comecador = "https://pngimage.net/wp-content/uploads/2018/06/game-play-button-png-3.png"
 
 
 class Registro:
@@ -37,26 +40,45 @@ class Registro:
         """ testa se após passar por aqui, self.nome se torna true e a variável grava o nome """
         #print(self.nome, self.nome_personagem) 
         
-VAI = Registro()
-VAI.registra()
+#VAI = Registro() 
+#VAI.registra()
+#print(VAI.nome, VAI.nome_personagem)
 
-
-
-        
 class Inicia:
 
     def __init__(self, ticket = False):
+        self.ticket = False
+        self.cena = Cena(tela_branca)
+        self.bot1 = Elemento(comecador, tit="CLIQUE PARA COMEÇAR",
+                           style=dict(height=100,widht=100, left=150, top=125), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = self.cena,
+                           vai = self.interacao_amais)
+        self.bot2 =Elemento(registrador, tit="CLIQUE PARA REGISTRAR",
+                           style=dict(height=100,widht=100, left=300, top=125), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = self.cena,
+                           vai = self.cria_usuario)
         self.ticket = ticket
-        if self.ticket == True:
-            Modulo1().mari()
         
-        
-    def cria_usuario(self):
-        TESTE = Registro().registra()
-        if nome== True:
+    def cria_usuario(self, *_):
+        INSTANCIA = Registro()
+        INSTANCIA.registra()
+        if INSTANCIA.nome == True:
             self.ticket = True
+            self.chama_mod1()
+            
+    def interacao_amais(self):
+        self.nome_automatico = "Fulana"
+        print(self.nome_automatico)
+        self.chama_mod1()
             
             
-#if __name__ == "__main__":    
-    #Inicia().cria_usuario()
+    def chama_mod1(self):
+        Modulo1().mari()
+    
+    def gera(self):
+        self.cena.vai()
+            
+            
+if __name__ == "__main__":    
+    Inicia().gera()
     #Registro().registra()
