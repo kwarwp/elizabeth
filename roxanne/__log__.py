2480ca86bd1ec8df8af9d3b367dcf7e082a01378
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-23 18:09:54 -0300

    Saved roxanne/__log__.py @2020-12-23 21:09

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -1623,3 +1623,42 @@ Traceback (most recent call last):
   module roxanne.main line 15
     from stacy.main import Modulo1
 '''},
+{'date': 'Wed Dec 23 2020 18:09:54.305 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    from stacy.main import Modulo1
+  module stacy.main line 2
+    from roxanne.main import *
+  module roxanne.main line 16
+    from stacy.main import Modulo1
+ImportError: cannot import name 'Modulo1'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    from stacy.main import Modulo1
+  module stacy.main line 2
+    from roxanne.main import *
+  module roxanne.main line 16
+    from stacy.main import Modulo1
+'''},
