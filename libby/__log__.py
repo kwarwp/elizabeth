3fa935e150a0de04d514ca35c6de61514564cf65
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-05 13:20:41 -0300

    Saved libby/__log__.py @2021-01-05 16:20

--- libby/__log__.py
+++ libby/__log__.py
@@ -1088,3 +1088,21 @@ SyntaxError: non-keyword arg after keyword arg
     self.LIVRO= Elemento(imagem_livro, tit="Livro",
 AttributeError: 'desafio_2' object has no attribute 'QUARTO2'
 '''},
+{'date': 'Tue Jan 05 2021 13:20:43.773 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    class desafio_2:
+  module <module> line 73
+    self.LIVRO= Elemento(imagem_livro, tit="Livro",
+AttributeError: 'GUI' object has no attribute 'QUARTO2'
+'''},
