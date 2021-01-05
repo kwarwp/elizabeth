cde8fd68ceb0385d3585a268d88733728ab3d749
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-05 12:24:44 -0300

    Saved libby/__log__.py @2021-01-05 15:24

--- libby/__log__.py
+++ libby/__log__.py
@@ -1031,3 +1031,21 @@ NameError: name 'nome_personagem' is not defined
     self.cena1.vai()
 AttributeError: 'desafio_2' object has no attribute 'cena1'
 '''},
+{'date': 'Tue Jan 05 2021 12:24:47.182 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 87
+    desafio_2().inicia()
+  module <module> line 83
+    self.cena1.vai()
+AttributeError: 'desafio_2' object has no attribute 'cena1'
+'''},
