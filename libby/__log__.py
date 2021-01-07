511af3183ceaaaa13b2fc75ef25f0f8a2e22788f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-07 18:24:40 -0300

    Saved libby/__log__.py @2021-01-07 21:24

--- libby/__log__.py
+++ libby/__log__.py
@@ -1131,3 +1131,21 @@ AttributeError: 'desafio_2' object has no attribute 'QUARTO2'
                                                                                                                                                                                                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jan 07 2021 18:24:39.824 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    class desafio_2:
+  module <module> line 64
+    self.texto_2= Texto(self.cena2, 
+AttributeError: 'GUI' object has no attribute 'cena2'
+'''},
