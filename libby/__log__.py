2aa76685c3e565472a1f3a34d5eeed377324e2e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-08 17:59:23 -0300

    Saved libby/__log__.py @2021-01-08 20:59

--- libby/__log__.py
+++ libby/__log__.py
@@ -1350,3 +1350,34 @@ AttributeError: 'GUI' object has no attribute 'cena2'
                                                                                                                                                                                                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jan 08 2021 17:59:22.762 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from meredith.main import nome_personagem
+ImportError: cannot import name 'nome_personagem'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from meredith.main import nome_personagem
+'''},
