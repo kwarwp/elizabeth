db65de0a6a34784b6245c7e22f693f27deeb8681
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-08 11:48:19 -0300

    Saved libby/__log__.py @2021-01-08 14:48

--- libby/__log__.py
+++ libby/__log__.py
@@ -1271,3 +1271,21 @@ IndentationError: unexpected indent
     self.BOTAO_DESAFIO1= Elemento(botao_desafio1, tit="PLAY",
 AttributeError: 'GUI' object has no attribute 'cena2'
 '''},
+{'date': 'Fri Jan 08 2021 11:48:19.213 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    class desafio_2:
+  module <module> line 85
+    self.BOTAO_DESAFIO1= Elemento(botao_desafio1, tit="PLAY",
+AttributeError: 'GUI' object has no attribute 'cena2'
+'''},
