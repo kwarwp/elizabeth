2cdd9393cd8c7f088a051d55f6a6bfae50a233bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-12 22:56:16 -0300

    Created lisa/__log__.py @2021-01-13 01:56

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Jan 12 2021 22:56:12.450 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  """ A multipla escolha é um popup com opções
+  ^
+IndentationError: unexpected indent
+'''},
