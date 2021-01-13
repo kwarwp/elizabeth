0afa40d188adad8b67d91fa50786a75a95c94ca9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-12 23:16:06 -0300

    Saved lisa/main.py @2021-01-13 02:16

--- lisa/main.py
+++ lisa/main.py
@@ -22,7 +22,7 @@ def resultado(A):
     devolutiva.vai()
 
 
-pergunta = Texto(nome_da_cena, txt = "Qual é a resposta para o desafio?", foi = resultado, A= MEU_ELEMENTO, B= "resposta")
+pergunta = Texto(nome_da_cena, txt = "Qual é a resposta para o desafio?", foi = resultado, A= MEU_ELEMENTO, B= "resposta", c= 'lalala')
 
 nome_da_cena.vai()
 pergunta.vai()
