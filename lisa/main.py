9e53a1fa98c909f5917421dff2b77e80e35d65a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-12 23:05:09 -0300

    Saved lisa/main.py @2021-01-13 02:05

--- lisa/main.py
+++ lisa/main.py
@@ -1,7 +1,7 @@
 # elizabeth.lisa.main.py
 
 
-from _spy.vitollino.main import Cena, Texto, STYLE
+from _spy.vitollino.main import Cena, Texto, STYLE, Elemento
 
 STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
