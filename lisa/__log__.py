366e0344af25d90ce00d9cdfe7d0db84e8b15309
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-12 23:02:11 -0300

    Saved lisa/__log__.py @2021-01-13 02:02

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -70,3 +70,19 @@ NameError: name 'Elemento' is not defined
     STYLE["width"] = 960 #  width = 300 (default)
 NameError: name 'STYLE' is not defined
 '''},
+{'date': 'Tue Jan 12 2021 23:02:08.459 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    RESPOSTA1= Elemento(MEU_ELEMENTO, tit="PLAY", w=55,h=58, x=610, y=300, cena = nome_da_cena)
+NameError: name 'Elemento' is not defined
+'''},
