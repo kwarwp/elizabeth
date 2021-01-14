fd63c81aca14ba4a09c2096c570e4a2a2d1e39b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-14 20:49:35 -0300

    Saved lisa/main.py @2021-01-14 23:49

--- lisa/main.py
+++ lisa/main.py
@@ -2,18 +2,17 @@
 
 
 from _spy.vitollino.main import Cena, Texto, STYLE, Elemento
+from PIL import Image
 
 STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 
 
 MINHA_CENA = "https://i.imgur.com/CsOVXPc.jpeg" # Extensões aceitas: png, jpg, jpeg e gif
-MEU_ELEMENTO = "https://i.imgur.com/aYRC7bM.png" # Extensões aceitas: png, jpg, jpeg e gif
+MEU_ELEMENTO =Imagem.open( "https://i.imgur.com/aYRC7bM.png") # Extensões aceitas: png, jpg, jpeg e gif
 
 nome_da_cena = Cena(MINHA_CENA)
 
-
-RESPOSTA1= Elemento(MEU_ELEMENTO, tit="PLAY", w=55,h=58, x=610, y=300, cena = nome_da_cena)
 
 def resultado(A):
      # O novo popupque será gerado quando o foi() do texto forchamado
