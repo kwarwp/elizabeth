3cc6d6f9c63cba622c099b6f80b71454b65b44c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-19 18:18:37 -0300

    Saved ruzwana/__log__.py @2021-01-19 21:18

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -228,3 +228,19 @@ NameError: name 'TUBARAO_VIVO' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Jan 19 2021 18:18:37.246 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    desafio_3().inicia()
+AttributeError: 'desafio_3' object has no attribute 'inicia'
+'''},
