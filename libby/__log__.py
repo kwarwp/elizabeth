9a5997f1770ada2337545bb3f3642e0fb80e99e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-20 22:32:29 -0300

    Saved libby/__log__.py @2021-01-21 01:32

--- libby/__log__.py
+++ libby/__log__.py
@@ -1446,3 +1446,21 @@ NameError: name 'boneca1' is not defined
     texto_3.vai()
 NameError: name 'texto_3' is not defined
 '''},
+{'date': 'Wed Jan 20 2021 22:31:30.109 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    desafio_2().inicia()
+  module <module> line 63
+    self.texto_3= Texto(QUARTO, txt= '{nome_personagem} gosta de ler um livro quando acorda.')
+NameError: name 'QUARTO' is not defined
+'''},
