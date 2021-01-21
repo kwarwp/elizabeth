f0b6f070839d999a7a8fe5f9421cd2e086180d1b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-20 22:12:58 -0300

    Saved libby/main.py @2021-01-21 01:12

--- libby/main.py
+++ libby/main.py
@@ -57,7 +57,7 @@ class desafio_2:
                              w=55,h=58, x=900, y=420,
                              cena = self.QUARTO)
         self.BONECA1 = Elemento(imagem_boneca1, tit="Onde pode estar meu livro?",
-                                w=10,h=58, x=900, y=420,
+                                w=55,h=58, x=900, y=420,
                                 cena = self.QUARTO)
         
         self.SETA.elt.bind("click", self.funcao_de_acao_do_botao3)  
