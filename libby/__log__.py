bfa4f933a13bb76b1ab997c6b8ab7d83fdd7959d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-20 22:29:02 -0300

    Saved libby/__log__.py @2021-01-21 01:29

--- libby/__log__.py
+++ libby/__log__.py
@@ -1430,3 +1430,19 @@ Traceback (most recent call last):
     self.BONECA1 = Elemento(imagem = boneca1, tit="Onde pode estar meu livro?",
 NameError: name 'boneca1' is not defined
 '''},
+{'date': 'Wed Jan 20 2021 22:28:03.310 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 116
+    texto_3.vai()
+NameError: name 'texto_3' is not defined
+'''},
