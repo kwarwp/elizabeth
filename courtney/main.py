7f72954fe7cd1733c484762dfb902c6ca20de804
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-23 11:45:24 -0300

    Saved courtney/main.py @2021-01-23 14:45

--- courtney/main.py
+++ courtney/main.py
@@ -8,46 +8,59 @@ porta_aberta="https://image.freepik.com/vetores-gratis/porta-aberta-dos-desenhos
 Imagem_Biblioteca_dentro="https://secult.es.gov.br/Media/secult/BPES/IMG_7650.JPG"
 imagem_caixa="https://thumbs.dreamstime.com/z/caixa-dos-desenhos-animados-no-fundo-branco-98137796.jpg"
 imagem_papel="https://thumbs.dreamstime.com/z/folhas-de-pap%C3%A9is-de-nota-com-o-desenho-da-m%C3%A3o-do-pino-do-impulso-89420296.jpg"
-STYLE["width"] = 500 #  width = 300 (default)
-STYLE["heigth"] = '600px' # min-height = "300px"
-
+Imagem_botao="https://images-na.ssl-images-amazon.com/images/I/71nQDXqkyDL.png"
 
 STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 
-Biblioteca_entrada=Cena(porta_fechada)
-Biblioteca_entrada.vai()
 
-Biblioteca_porta=Cena(porta_aberta)
+class desafio_2:
 
-Biblioteca_dentro=Cena(Imagem_Biblioteca_dentro)
+    def __init__(self):
 
-
-Imagem_botao="https://images-na.ssl-images-amazon.com/images/I/71nQDXqkyDL.png"
-
-def abre_porta (event = None):
-    Biblioteca_porta.vai()
-    
-BOTAO= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                cena = Biblioteca_entrada)
+        self.Biblioteca_entrada=Cena(porta_fechada)
+        
+        self.Biblioteca_porta=Cena(porta_aberta)
+        
+        self.Biblioteca_dentro=Cena(Imagem_Biblioteca_dentro)
+        
+        self.BOTAO= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                             cena = self.Biblioteca_entrada)
                                 
-BOTAO.elt.bind("click", abre_porta)
-
-def entra_na_biblioteca (event = None):
-    Biblioteca_dentro.vai()
-    
-BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                cena = Biblioteca_porta)
+        self.BOTAO.elt.bind("click", self.abre_porta)
+        
+        self.BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                              cena = self.Biblioteca_porta)
+                              
+        self.BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                              cena = self.Biblioteca_porta)
                                 
-BOTAO2.elt.bind("click", entra_na_biblioteca)
+        self.BOTAO2.elt.bind("click", self.entra_na_biblioteca)
+        
+        self.caixa_abre = Cena(imagem_papel)
+        
+        self.botao_caixa = Elemento(imagem_caixa, tit="CLIQUE",
+                           style=dict(height=100,widht=100, left=300, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = self.Biblioteca_dentro,
+                           vai = self.clique_caixa)
 
-            
-def clique_caixa (event = None):
-    caixa_abre = Cena(imagem_papel)
-    caixa_abre.vai()
+
+    def abre_porta (self,event = None):
+        self.Biblioteca_porta.vai()
     
-botao_caixa = Elemento(imagem_caixa, tit="CLIQUE",
-                       style=dict(height=100,widht=100, left=300, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                       cena = Biblioteca_dentro,
-                       vai = clique_caixa)
+
+
+    def entra_na_biblioteca (self,event = None):
+        self.Biblioteca_dentro.vai()
+    
+
+
+    def clique_caixa (self,event = None):
+        self.caixa_abre.vai()
+    
+    def inicia(self,*_):
+        self.Biblioteca_entrada.vai()
+        
+if __name__ == "__main__":                  
+    desafio_2().inicia()
 
