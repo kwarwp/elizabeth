c8192eaae93fc7500855e88a3d03b103746cb540
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-24 14:01:12 -0300

    Saved libby/__log__.py @2021-01-24 17:01

--- libby/__log__.py
+++ libby/__log__.py
@@ -1503,3 +1503,21 @@ IndentationError: expected an indented block
     self.Entrada1 = Cena(imagem_computador1)
 NameError: name 'imagem_computador1' is not defined
 '''},
+{'date': 'Sun Jan 24 2021 14:00:09.55 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 138
+    desafio_1().inicia()
+  module <module> line 55
+    self.SetaEntrada = Elemento(botao_seta, tit="CLIQUE",
+AttributeError: 'desafio_1' object has no attribute 'clique_computador'
+'''},
