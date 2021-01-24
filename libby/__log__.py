aa74c51a1728fa78506c0dd2ea49ed72aff214f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-24 13:58:55 -0300

    Saved libby/__log__.py @2021-01-24 16:58

--- libby/__log__.py
+++ libby/__log__.py
@@ -1485,3 +1485,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Jan 24 2021 13:57:52.448 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    desafio_1().inicia()
+  module <module> line 53
+    self.Entrada1 = Cena(imagem_computador1)
+NameError: name 'imagem_computador1' is not defined
+'''},
