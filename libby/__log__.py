96dfa1d105f8979a04aaef434916dc3ccaff97ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-25 20:29:34 -0300

    Saved libby/__log__.py @2021-01-25 23:29

--- libby/__log__.py
+++ libby/__log__.py
@@ -1714,3 +1714,21 @@ AttributeError: 'Cena' object has no attribute 'inicia'
     self.ENTRADA1().inicia()
 AttributeError: 'Cena' object has no attribute 'inicia'
 '''},
+{'date': 'Mon Jan 25 2021 20:28:30.250 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 154
+    desafio_1().inicia()
+  module <module> line 149
+    self.ENTRADA1().inicia()
+AttributeError: 'Cena' object has no attribute 'inicia'
+'''},
