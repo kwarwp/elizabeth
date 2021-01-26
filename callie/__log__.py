ecfba67fc5aeeac947068e828d93be15f12bd363
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-26 14:28:49 -0300

    Created callie/__log__.py @2021-01-26 17:28

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Jan 26 2021 14:28:47.774 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  def resgata(self,x,y,w,h):
+  ^
+IndentationError: unexpected indent
+'''},
