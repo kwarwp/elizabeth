b45de6e76d271dc010774e92e252b36c933b649f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-26 14:30:06 -0300

    Saved callie/__log__.py @2021-01-26 17:30

--- callie/__log__.py
+++ callie/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Jan 26 2021 14:30:05.720 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  """Aciona estado de inv.bota = True para que eventual clique devolva o Elemento para a cena"""
+  ^
+IndentationError: expected an indented block
+'''},
