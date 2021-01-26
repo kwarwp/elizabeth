bc0fc634fa5d9fe8689b127ec9fce0d59d7ee021
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-26 17:15:47 -0300

    Saved courtney/__log__.py @2021-01-26 20:15

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -127,3 +127,21 @@ AttributeError: 'desafio_2' object has no attribute 'Biblioteca_dentro'
     self.CAIXA_ABERTA("click", self.CAIXA_ABRE)
 TypeError: 'Elemento' object is not callable
 '''},
+{'date': 'Tue Jan 26 2021 17:15:47.754 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    desafio_2().inicia()
+  module <module> line 80
+    self.CAIXA_ABERTA("click", self.CAIXA_ABRE)
+TypeError: 'Elemento' object is not callable
+'''},
