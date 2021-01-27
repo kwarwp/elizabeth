3664fc87a77ee9c236f9d1c748af2eb75638e1eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-27 19:21:05 -0300

    Saved heather/main.py @2021-01-27 22:21

--- heather/main.py
+++ heather/main.py
@@ -53,13 +53,13 @@ class desafio_1:
                             w=55, h=58, x=500, y=390,
                             cena = self.ENTRADA2)
         self.BONECADORMINDO = Elemento(boneca_dormindo, tit="Acorde Hipátia",
-                              w=240, h=336, x=300, y=180,
+                              w=240, h=336, x=300, y=200,
                               cena = self.QUARTO1)
         self.SETA = Elemento(seta, tit="Próximo",
                              w=55,h=58, x=900, y=420,
                              cena = self.QUARTO)
         self.BONECA1 = Elemento(imagem_boneca1, tit="Onde pode estar meu livro?",
-                                w=300,h=420, x=600, y=200,
+                                w=240,h=336, x=300, y=200,
                                 cena = self.QUARTO)
         self.LIVRO= Elemento(imagem_livro, tit="É esse!",
                              w=55,h=58, x=850, y=390,
