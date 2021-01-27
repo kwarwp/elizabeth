2e6797767e324abe2dd497bf6226a6a02b25f079
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-01-27 19:11:21 -0300

    Created heather/__log__.py @2021-01-27 22:11

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jan 28 2021 01:10:13.546 GMt+0300 (GMT+03:00) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  boneca_dormindo = 'https://i.imgur.com/PMwusTO.png"
+                                                     ^
+SyntaxError: EOL while scanning string literal
+'''},
