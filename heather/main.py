43de99ede1364568ee80ba40fdf0e4fbb2f32e50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-01 21:54:19 -0300

    Saved heather/main.py @2021-02-02 00:54

--- heather/main.py
+++ heather/main.py
@@ -51,7 +51,7 @@ class desafio_1:
                             w=55, h=58, x=500, y=390,
                             cena = self.ENTRADA2)
         self.BONECADORMINDO = Elemento(boneca_dormindo, tit="Acorde Hipátia",
-                              w=336, h=240, x=300, y=250,
+                              w=336, h=240, x=250, y=300,
                               cena = self.QUARTO1)
         self.SETA = Elemento(seta, tit="Próximo",
                              w=55,h=58, x=900, y=420,
