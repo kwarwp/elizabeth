53eccfeedb9af0fc5e0e65bfca09e653c068a78a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-08 14:16:15 -0300

    Saved callie/main.py @2021-02-08 17:16

--- callie/main.py
+++ callie/main.py
@@ -14,29 +14,6 @@ triangulo_amarelo= 'https://i.imgur.com/g8TxYsH.png'
 desafio= 'https://i.imgur.com/0kfiusf.png'
 biblioteca = "https://i.imgur.com/wistRJZ.jpeg" 
 
-class Item_herdado(Elemento):
-    """Construção de uma classe que herde de Elemento
-    """
-    def bota(self, *_):
-        """Aciona estado de inv.bota = True para que eventual clique devolva o Elemento para a cena"""
-        inv.bota(self, True)
-        #self.vai=lambda*_:self.resgata(x=x,y=y,w=w,h=h)
-        """Método vai do Elemento atrelado ao evento de reposicionamento, onde o memento especifica os argumentos pedidos pelo método resgata."""
-        self.vai=lambda*_:self.resgata(*self.memento)
-
-    def resgata(self,x,y,w,h):
-        """Método para resgate do Elemento no inventário"""
-        self.x,self.y,self.w,self.h= x,y,w,h
-        """Retira Elemento atrelado ao título do inventário"""
-        inv.tira(self.tit)
-        """Coloca Elemento na cena"""
-        self.entra(inv.cena)
-        """Aciona estado de inv.bota = False para que eventual clique devolva o Elemento para o inventário"""
-        self.vai=self.bota
-
-    def mementor(self,memento):
-         """Permite que o style do elemento a ser recolocado na tela seja especificado"""
-         self.memento=memento
 
 
 class Main():
@@ -49,12 +26,20 @@ class Main():
                                w=300,h=300, x=310, y=280, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                                cena = self.BIBLIOTECA)
 
-        self.T_AZUL=Item_herdado(triangulo_azul, tit="nome_do_meu_elemento", 
+        self.T_AZUL=Elemento (triangulo_azul, tit="nome_do_meu_elemento", 
                                 H=60,w=60, X=100, Y=100,
                                 cena=self.BIBLIOTECA)
+                                
+        self.T_AMARELO=Elemento (triangulo_amarelo, tit="nome_do_meu_elemento", 
+                                H=60,w=60, X=200, Y=200,
+                                cena=self.BIBLIOTECA)
+                                
+        self.T_VERMELHO =Elemento (triangulo_vermelho, tit="nome_do_meu_elemento", 
+                                H=60,w=60, X=300, Y=300,
+                                cena=self.BIBLIOTECA)
         
-        self.T_AZUL.mementor((110,150,200,"300px"))
-        self.T_AZUL.vai=self.T_AZUL.bota
+        
+
 
         self.BIBLIOTECA.vai()
 
