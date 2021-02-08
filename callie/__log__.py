6aab37bd4aa1cddd41dd3a877120660875c2ef91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-08 17:42:52 -0300

    Saved callie/__log__.py @2021-02-08 20:42

--- callie/__log__.py
+++ callie/__log__.py
@@ -95,3 +95,21 @@ IndentationError: unexpected indent
     self.T_AMARELO5.elt.bind("click", self.desafio5)
 AttributeError: 'desafio_teorema' object has no attribute 'desafio5'
 '''},
+{'date': 'Mon Feb 08 2021 17:42:52.220 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 383
+    desafio_teorema().inicia()
+  module <module> line 245
+    self.DESAFIO7= Elemento(desafio, tit="Hipátia",
+AttributeError: 'desafio_teorema' object has no attribute 'BIBLIOTECA7'
+'''},
