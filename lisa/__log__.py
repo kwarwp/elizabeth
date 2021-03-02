2e95b4ae25eaf67c39bebbf7bc26c3f3ec126c30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-02 13:13:42 -0300

    Saved lisa/__log__.py @2021-03-02 16:13

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -299,3 +299,19 @@ IndentationError: expected an indented block
     self.RESOSTA_B.elt.bind("click", self.desafio1)
 AttributeError: 'desafio_quadrado' object has no attribute 'RESOSTA_B'
 '''},
+{'date': 'Tue Mar 02 2021 13:13:44.416 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    desafio_quadrado().inicia()
+AttributeError: 'desafio_quadrado' object has no attribute 'inicia'
+'''},
