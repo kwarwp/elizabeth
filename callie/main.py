096839c5ce61194c11388375ddbb6849ff954757
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-03 16:48:30 -0300

    Saved callie/main.py @2021-03-03 19:48

--- callie/main.py
+++ callie/main.py
@@ -7,14 +7,14 @@ from _spy.vitollino.main import INVENTARIO as inv
 STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 
-triangulo_azul = "https://i.imgur.com/IbG7St4.png"
-triangulo_verde= 'https://i.imgur.com/bcse41s.png'
-triangulo_vermelho= 'https://i.imgur.com/KAfuNJG.png'
+triangulo_azul = "https://i.imgur.com/1NvDXiC.png"
+triangulo_verde= 'https://i.imgur.com/f8e7Gkc.png'
+triangulo_vermelho= 'https://i.imgur.com/QaRil54.png'
 triangulo_amarelo= 'https://i.imgur.com/g8TxYsH.png'
-triangulo_roxo= 'http://1.bp.blogspot.com/_o7MZRBJ1zfQ/S3HPfxN6-FI/AAAAAAAADLI/f1i46TAmM4Q/s320/270px-Purple_triangle_svg+triangulo+roxo.png'
+triangulo_roxo= 'https://i.imgur.com/RsWbjkx.png'
 triangulo_rosa ='https://aventurasnahistoria.uol.com.br/media/uploads/pink_triangle_up.svg.png'
-triangulo_laranja= 'https://lh3.googleusercontent.com/proxy/-WJMuhgeeuzRRxS-nIgZ9PahHXP72kxbnoetbIMvrHxUy1RHrV-Ds7MzNuudP4rIav5ctJ0ELzdYb3EfdrkwKjxzcppd_-WMgtcMjIQ6FWRHD5CcWBMITK-QEz8CAHXIhMCfqC4zlYIKQO1sw0o'
-triangulo_ciano='https://cdn.pixabay.com/photo/2017/09/01/09/22/triangle-2703515_1280.jpg'
+triangulo_laranja= "https://i.imgur.com/TP6Fk9b.png"
+triangulo_ciano='https://i.imgur.com/wBVf9VF.png'
 quadrado_vermelho= 'https://thumbs.dreamstime.com/b/fundo-do-teste-padr%C3%A3o-quadrado-vermelho-110087135.jpg'
 desafio= 'https://i.imgur.com/0kfiusf.png'
 biblioteca = "https://i.imgur.com/wistRJZ.jpeg" 
