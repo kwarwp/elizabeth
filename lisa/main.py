f3e650e043fe476af56cd0bcdfd295ed1fe1583a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-03 17:05:46 -0300

    Saved lisa/main.py @2021-03-03 20:05

--- lisa/main.py
+++ lisa/main.py
@@ -7,6 +7,7 @@ STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 
 corredor= "https://i.imgur.com/oO8m0Bq.png"
+corredor2="https://i.imgur.com/YzJB15Y.png"
 resposta_a= "https://i.imgur.com/Ny4x06n.png"
 resposta_b= "https://i.imgur.com/B28Eot5.png"
 resposta_c= "https://i.imgur.com/FWrKaip.png"
@@ -16,7 +17,7 @@ class desafio_quadrado():
 
     def __init__(self):
         self.CORREDOR= Cena(corredor)
-        #self.CORREDOR2= Cena()
+        self.CORREDOR2= Cena(corredor2)
         
         #ELEMENTOS
         self.RESPOSTA_A= Elemento(resposta_a, tit="Click",
@@ -33,16 +34,16 @@ class desafio_quadrado():
         
         self.texto_1= Texto(self.CORREDOR, txt= 'Ao tentar fugir, Hipátia se deparou com um dessafio. Resolvá-o encaixando a peça correta')
         self.texto_2= Texto(self.CORREDOR, txt= 'Esta não é a peça correta.')
-        #self.texto_3= Texto(self.CORREDOR2, txt= 'Parabéns! Você concluiu o desafio!')
+        self.texto_3= Texto(self.CORREDOR2, txt= 'Parabéns! Você concluiu o desafio!')
         #botao
     
-        #self.RESOSTA_B.elt.bind("click", self.desafio1)
+        self.RESPOSTA_B.elt.bind("click", self.desafio1)
         self.RESPOSTA_A.elt.bind("click", self.botao_errado)
         self.RESPOSTA_C.elt.bind("click", self.botao_errado)
     
-      # def desafio1(self,*_):
-         #  self.CORREDOR2.vai()
-       # self.texto_3.vai()
+    def desafio1(self,*_):
+        self.CORREDOR2.vai()
+        self.texto_3.vai()
         
     def botao_errado(self,*_):
         self.texto_2.vai()
