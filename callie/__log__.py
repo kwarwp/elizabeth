1b8317936b12f4c6e2d15d852a199b7b707d74d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-16 21:25:13 -0300

    Saved callie/__log__.py @2021-03-17 00:25

--- callie/__log__.py
+++ callie/__log__.py
@@ -185,3 +185,21 @@ AttributeError: 'desafio_teorema' object has no attribute 'Biblioteca_porta'
     self.BOTAO.elt.bind("click", self.abre_porta)
 AttributeError: 'desafio_teorema' object has no attribute 'BOTAO'
 '''},
+{'date': 'Tue Mar 16 2021 21:25:13.586 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 396
+    desafio_teorema().inicia()
+  module <module> line 333
+    self.PORTA.elt.bind("click", self.abre_porta)
+AttributeError: 'desafio_teorema' object has no attribute 'PORTA'
+'''},
