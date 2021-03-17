285fd7e34b0f34b84a54ca8b7823291908746f3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-16 21:24:25 -0300

    Saved callie/__log__.py @2021-03-17 00:24

--- callie/__log__.py
+++ callie/__log__.py
@@ -149,3 +149,21 @@ NameError: name 'quadrado_vermelho' is not defined
     self.BONECA2= Elemento(imagem_boneca2, tit="Hipátia",
 NameError: name 'imagem_boneca2' is not defined
 '''},
+{'date': 'Tue Mar 16 2021 21:24:25.135 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 396
+    desafio_teorema().inicia()
+  module <module> line 266
+    self.texto_2_2=Texto(self.Biblioteca_porta, txt= "A porta esta trancada. Resolva o desafio para abri-la" )
+AttributeError: 'desafio_teorema' object has no attribute 'Biblioteca_porta'
+'''},
