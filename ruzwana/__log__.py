b798b3a6864c3608ae51286e156d79e8388d605a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-23 20:27:35 -0300

    Saved ruzwana/__log__.py @2021-03-23 23:27

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -303,3 +303,21 @@ SyntaxError: keyword can't be an expression
     self.PASTA_ABERTA(pasta_aberta)
 AttributeError: 'desafio_3' object has no attribute 'PASTA_ABERTA'
 '''},
+{'date': 'Tue Mar 23 2021 20:27:24.178 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    desafio_3().inicia()
+  module <module> line 34
+    self.PLAY = Elemento(botao_play, tit="PLAY",
+NameError: name 'botao_play' is not defined
+'''},
