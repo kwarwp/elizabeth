0213c10abe6bcff0765517b82cd89e7657f9e81d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-23 20:45:45 -0300

    Saved callie/__log__.py @2021-03-23 23:45

--- callie/__log__.py
+++ callie/__log__.py
@@ -221,3 +221,21 @@ AttributeError: 'desafio_teorema' object has no attribute 'PORTA'
     self.CONTINUA = Elemento(seta, tit="Próximo",
 NameError: name 'seta' is not defined
 '''},
+{'date': 'Tue Mar 23 2021 20:45:21.218 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 414
+    desafio_teorema().inicia()
+  module <module> line 290
+    self.texto_10 = Texto(self.cena2, txt = "O desafio é um quebra cabeça. Clique na peça que compõe a imagem corretamente para completá-lo.")
+AttributeError: 'desafio_teorema' object has no attribute 'cena2'
+'''},
