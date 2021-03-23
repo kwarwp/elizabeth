e7814e52f38a27c47c47159bb482939a1cf4bff5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-23 20:32:36 -0300

    Saved courtney/__log__.py @2021-03-23 23:32

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -166,3 +166,21 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Mar 23 2021 20:32:11.595 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 293
+    desafio_2().inicia()
+  module <module> line 221
+    self.texto_5 = Texto(self.cena2, txt = "O desafio é um anagrama. Um anagrama é criado a partir de uma palavra com suas letras embaralhadas. Tente reorganizar a frase para encontrar o recado e resolver o desafio.")
+AttributeError: 'desafio_2' object has no attribute 'cena2'
+'''},
