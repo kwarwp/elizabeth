23c15cc604ed2dc69bdff95610ef4949258613ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-24 16:13:21 -0300

    Saved ruzwana/main.py @2021-03-24 19:13

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -16,6 +16,9 @@ coordenadas_rj= "https://i.imgur.com/dKLFxWB.png"
 mapa= "https://i.imgur.com/E2MZ6DR.png"
 click_biblioteca= 'https://i.imgur.com/ZKiFXHh.png'
 click_cabana="https://i.imgur.com/ueEjt2c.png"
+pasta= "https://i.imgur.com/KeffUYv.png"
+jornal= "https://i.imgur.com/WuKy09p.png"
+carta_aberta= "https://i.imgur.com/CiQ67ph.png"
 
 imagem_boneca2 = 'https://i.imgur.com/NEyFwDm.png'
 
@@ -29,6 +32,7 @@ class desafio_3:
         self.ENTRADA_ADM = Cena(porta_adm)        
         self.ADM= Cena(sala_adm)
         self.ADM2= Cena(sala_adm)
+        self.ADM3= Cena(sala_adm)
         self.DESAFIO_CODIGO = Cena(imagem_desafiocodigo)
         self.COMPUTADOR = Cena(computador)
         self.COMPUTADOR2 = Cena(computador)
@@ -37,7 +41,7 @@ class desafio_3:
         self.PORTA1= Elemento(porta, tit="Abra a porta",
                              w=200,h=400,  x=460, y=70, 
                              cena = self.ENTRADA_ADM)
-        self.PASTA= Elemento(pasta_confidencial, tit="Abra a pasta",
+        self.PASTA_FECHADA= Elemento(pasta_confidencial, tit="Abra a pasta",
                              w=50,h=20,  x=380, y=400, 
                              cena = self.ADM)
         self.PLAY = Elemento(botao_play, tit="PLAY",
@@ -59,6 +63,10 @@ class desafio_3:
                                w=300,h=400, x=100, y=240, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                                cena = self.ADM2) 
                                
+        self.BONECA4= Elemento(imagem_boneca2, tit="Hipátia",
+                               w=300,h=400, x=100, y=240, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                               cena = self.ADM3) 
+                               
         self.SENHA= Elemento(coordenadas, tit="Coordenada",
                                w=500,h=264, x=240, y=140, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                                cena = self.COMPUTADOR)  
@@ -77,22 +85,46 @@ class desafio_3:
                               w=55,h=58, x=250, y=500, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                               cena = self.MAPA)  
                               
+                              
+        self.PASTA_ABERTA= Elemento (pasta, tit="pasta",
+                                     w=700, h=450, x=100, y=100,
+                                     cena= self.ADM2)
+                                     
+        self.PASTA_ABERTA2= Elemento (pasta, tit="pasta",
+                                      w=700, h=450, x=100, y=100,
+                                      cena= self.ADM3)
+                              
         self.CARTA= Elemento (carta, tit="carta",
-                              w=303, h=216, x=240, y=140,
+                              w=300, h=200, x=480, y=240,
                               cena= self.ADM2)
+                              
+        self.CARTA_ABERTA= Elemento (carta_aberta, tit="carta",
+                                     w=300, h=450, x=480, y=100,
+                                     cena= self.ADM3)
+                                     
+        self.JORNAL= Elemento(jornal, tit= "Jornal",
+                              w=350, h=300, x=50, y=150,
+                              cena=self.ADM2)
+        
+        self.JORNAL2= Elemento(jornal, tit= "Jornal",
+                              w=350, h=300, x=50, y=150,
+                              cena=self.ADM3)
+                              
+        
 
         
         self.PORTA1.elt.bind("click", self.abre_porta)
-        self.PASTA.elt.bind("click", self.abre_pasta)
+        self.PASTA_FECHADA.elt.bind("click", self.abre_pasta)
         self.PLAY.elt.bind("click", self.desafio_codigo)
         self.SETA.elt.bind("click", self.botao_seguir)
         self.SENHA.elt.bind("click", self.desafio4)
         self.mini_mapa.elt.bind("click", self.abre_mapa)
+        self.CARTA.elt.bind("click", self.abre_carta)
         
         self.texto_1=Texto(self.ENTRADA_ADM, txt = 'Parabéns, Hipátia! Você conseguiu escapar. Entre na diretoria da biblioteca para mais informações')
         self.texto_2=Texto(self.ADM, txt = 'Encontre a pasta confidencial.')
-        self.texto_3=Texto(self.ADM2, txt = "Após ler as informações, Hipátia percebeu que havia um código para ser resolvido. Clique no botão para descobrir o que é.")
-        self.texto_4=Texto(self.DESAFIO_CODIGO, txt = "Descubra o código e anote-o. Hipátia precisará dele para seu próximo passo.")
+        self.texto_3=Texto(self.ADM2, txt = "Após ler as informações, Hipátia percebeu que havia uma carta. Clique para descobrir o que esta escrito.")
+        self.texto_4=Texto(self.ADM3, txt = "Descubra o código e anote-o. Hipátia precisará dele para seu próximo passo.")
         self.texto_5=Texto(self.COMPUTADOR, txt = "Insira o código descoberto na última etapa.")
         
     def botao_seguir (self, event = None):
@@ -101,6 +133,9 @@ class desafio_3:
         
     def abre_mapa (self, event= None):
         self.MAPA.vai()
+    
+    def abre_carta (self, event= None):
+        self.ADM3.vai()
     
     def desafio4 (self, event = None):
         self.digite_senha= input("Digite a senha do computador para liberar a coordenada.")
