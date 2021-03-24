2deaebcb699eb2915e4f2a89dab13af3490c88d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-24 20:03:34 -0300

    Saved courtney/__log__.py @2021-03-24 23:03

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -220,3 +220,21 @@ AttributeError: 'desafio_2' object has no attribute 'imagem_papel'
     self.INFOS = Elemento(infos_1, tit='',
 AttributeError: 'desafio_2' object has no attribute 'imagem_papel'
 '''},
+{'date': 'Wed Mar 24 2021 20:03:08.337 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 307
+    desafio_2().inicia()
+  module <module> line 189
+    self.informacoes.elt.bind("click", self.funcao_de_acao_do_botao4)
+AttributeError: 'desafio_2' object has no attribute 'informacoes'
+'''},
