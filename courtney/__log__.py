e1a9d47993dc78a25448091e2fc3ef82f434469e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-24 19:34:32 -0300

    Saved courtney/__log__.py @2021-03-24 22:34

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -184,3 +184,21 @@ IndentationError: unexpected indent
     self.texto_5 = Texto(self.cena2, txt = "O desafio é um anagrama. Um anagrama é criado a partir de uma palavra com suas letras embaralhadas. Tente reorganizar a frase para encontrar o recado e resolver o desafio.")
 AttributeError: 'desafio_2' object has no attribute 'cena2'
 '''},
+{'date': 'Wed Mar 24 2021 19:34:06.609 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 302
+    desafio_2().inicia()
+  module <module> line 156
+    self.INFOS = Elemento(infos_1, tit='',
+AttributeError: 'desafio_2' object has no attribute 'imagem_papel'
+'''},
