7c366a03376bb4e228eff06aea3a522b4ba1429b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-24 20:42:49 -0300

    Saved adda/__log__.py @2021-03-24 23:42

--- adda/__log__.py
+++ adda/__log__.py
@@ -65,3 +65,21 @@ AttributeError: 'ATO4' object has no attribute 'vai'
     ATO4().inicia()                    
 AttributeError: 'ATO4' object has no attribute 'inicia'
 '''},
+{'date': 'Wed Mar 24 2021 20:42:23.168 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    ATO4().inicia()                    
+  module <module> line 42
+    self.HIPATIA_COSTAS.elt.bind("click", self.ENTROU)
+AttributeError: 'ATO4' object has no attribute 'ENTROU'
+'''},
