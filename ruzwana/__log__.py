760f2610e319779c96165b05cd90c03fed55d559
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-24 15:22:50 -0300

    Saved ruzwana/__log__.py @2021-03-24 18:22

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -389,3 +389,21 @@ SyntaxError: invalid syntax
     self.PASTA_ABERTA= Cena(pasta_aberta)
 NameError: name 'pasta_aberta' is not defined
 '''},
+{'date': 'Wed Mar 24 2021 15:22:49.149 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 128
+    desafio_3().inicia()
+  module <module> line 43
+    self.PLAY = Elemento(botao_play, tit="PLAY",
+AttributeError: 'desafio_3' object has no attribute 'PASTA_ABERTA'
+'''},
