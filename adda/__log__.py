36d6354eae9fb445ab225a8d41bfa7c16c26ccdb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-25 16:27:57 -0300

    Saved adda/__log__.py @2021-03-25 19:27

--- adda/__log__.py
+++ adda/__log__.py
@@ -101,3 +101,21 @@ AttributeError: 'ATO4' object has no attribute 'ENTROU'
     self.texto_2 = Texto(self.CABANA_DENTRO, txt= "Ao entrar, Hipátia se deparou com alguém e se espantou. Era sua mãe. Clique na mãe da Hipátia para saber o que ela tem a dizer.")
 AttributeError: 'ATO4' object has no attribute 'CABANA_DENTRO'
 '''},
+{'date': 'Thu Mar 25 2021 16:27:56.372 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    ATO4().inicia()                    
+  module <module> line 32
+    self.ABRE_PORTA.elt.bind("click", self.PORTA_ABRE)
+AttributeError: 'ATO4' object has no attribute 'ABRE_PORTA'
+'''},
