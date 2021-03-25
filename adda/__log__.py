b335f03eb65a4064b2f235bc94809d68257bd304
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-25 16:20:48 -0300

    Saved adda/__log__.py @2021-03-25 19:20

--- adda/__log__.py
+++ adda/__log__.py
@@ -83,3 +83,21 @@ AttributeError: 'ATO4' object has no attribute 'inicia'
     self.HIPATIA_COSTAS.elt.bind("click", self.ENTROU)
 AttributeError: 'ATO4' object has no attribute 'ENTROU'
 '''},
+{'date': 'Thu Mar 25 2021 16:20:47.418 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    ATO4().inicia()                    
+  module <module> line 48
+    self.texto_2 = Texto(self.CABANA_DENTRO, txt= "Ao entrar, Hipátia se deparou com alguém e se espantou. Era sua mãe. Clique na mãe da Hipátia para saber o que ela tem a dizer.")
+AttributeError: 'ATO4' object has no attribute 'CABANA_DENTRO'
+'''},
