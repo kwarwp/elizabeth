5981c4ab997c8b21da87eb10dc8c1537cfb63645
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-25 16:31:23 -0300

    Saved adda/__log__.py @2021-03-25 19:31

--- adda/__log__.py
+++ adda/__log__.py
@@ -119,3 +119,21 @@ AttributeError: 'ATO4' object has no attribute 'CABANA_DENTRO'
     self.ABRE_PORTA.elt.bind("click", self.PORTA_ABRE)
 AttributeError: 'ATO4' object has no attribute 'ABRE_PORTA'
 '''},
+{'date': 'Thu Mar 25 2021 16:31:22.59 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    ATO4().inicia()                    
+  module <module> line 28
+    self.SETA_1 = Elemento(imagem_seta1, tit="CLIQUE",
+AttributeError: 'ATO4' object has no attribute 'CABANA_DENTRO'
+'''},
