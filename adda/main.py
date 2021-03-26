060873069f32bfc0fab977e67d90d4f7c87b4b23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-26 17:03:47 -0300

    Saved adda/main.py @2021-03-26 20:03

--- adda/main.py
+++ adda/main.py
@@ -6,9 +6,10 @@ from _spy.vitollino.main import Cena, Elemento, STYLE, Texto, Sala
 imagem_cabanafora = 'https://i.imgur.com/3qeO1qP.jpg'
 imagem_porta = 'https://i.imgur.com/f1yocCX.png'
 imagem_cabanadentro1 = 'https://i.imgur.com/r7WL6jW.jpeg'
-imagem_cabanadentro2 = 'https://i.imgur.com/OZGjgtL.png'
-imagem_cabanadentro3 = 'https://i.imgur.com/OZGjgtL.png'
-imagem_cabanadentro4 = 'https://i.imgur.com/OZGjgtL.png'
+imagem_cabanadentro2 = 'https://i.imgur.com/O610LO7.jpeg'
+imagem_cabanadentro3 = 'https://i.imgur.com/ax5lNR8.jpeg'
+imagem_cabanadentro4 = 'https://i.imgur.com/5eWOp3i.jpeg'
+imagem_cabanadentro5 = 'https://i.imgur.com/qx1sdZ8.jpeg'
 imagem_reuniao = 'https://i.imgur.com/tzvaPBC.jpeg'
 imagem_seta1 = 'https://image.flaticon.com/icons/png/512/37/37758.png'
 
@@ -26,6 +27,7 @@ class ATO4:
         self.CABANA_DENTRO2 = Cena(imagem_cabanadentro2)
         self.CABANA_DENTRO3 = Cena(imagem_cabanadentro3)
         self.CABANA_DENTRO4 = Cena(imagem_cabanadentro4)
+        self.CABANA_DENTRO5 = Cena(imagem_cabanadentro5)
         self.SALA_REUNIAO = Cena(imagem_reuniao)
         
         
@@ -49,18 +51,24 @@ class ATO4:
                               w=55,h=58, x=900, y=420,
                               cena = self.CABANA_DENTRO4)
                               
+        self.SETA_5 = Elemento(imagem_seta1, tit="CLIQUE",
+                              w=55,h=58, x=900, y=420,
+                              cena = self.CABANA_DENTRO5)
+                              
         self.texto_1 = Texto(self.CABANA_FORA, txt= "Clique na porta para entrar na cabana.")
         
         self.PORTA.elt.bind("click", self.PORTA_ABRE)
         self.SETA_1.elt.bind("click", self.DIALOGO1)
         self.SETA_2.elt.bind("click", self.DIALOGO2)
         self.SETA_3.elt.bind("click", self.DIALOGO3)
-        self.SETA_4.elt.bind("click", self.REUNIAO)
+        self.SETA_4.elt.bind("click", self.DIALOGO4)
+        self.SETA_5.elt.bind("click", self.REUNIAO)
 
         
                              
     def inicia(self,*_):
         self.CABANA_FORA.vai()
+        self.texto_1.vai()
         
     def PORTA_ABRE(self,*_):
         self.CABANA_DENTRO1.vai()       
@@ -73,6 +81,9 @@ class ATO4:
         
     def DIALOGO3(self,*_):
         self.CABANA_DENTRO4.vai()
+        
+    def DIALOGO4(self,*_):
+        self.CABANA_DENTRO5.vai()
         
     def REUNIAO(self,*_):
         self.SALA_REUNIAO.vai()
